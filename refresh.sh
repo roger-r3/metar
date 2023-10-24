1277a5c08604a2ca47eb3b31d635b4e9fa39848d
roger-r3 <87670075+roger-r3@users.noreply.github.com>
2023-10-24 10:58:09 -0700

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,3 @@
-/usr/bin/sudo pkill -F /home/pi/offpid.pid
-/usr/bin/sudo pkill -F /home/pi/metarpid.pid
-/usr/bin/sudo /usr/bin/python3 /home/pi/metar.py & echo $! > /home/pi/metarpid.pid
+/usr/bin/sudo pkill -F /home/pi/metar/offpid.pid
+/usr/bin/sudo pkill -F /home/pi/metar/metarpid.pid
+/usr/bin/sudo /usr/bin/python3 /home/pi/metar/metar.py & echo $! > /home/pi/metar/metarpid.pid
